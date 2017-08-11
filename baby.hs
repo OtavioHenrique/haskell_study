d9478dbc57879f5c9c0a4fbd4a081abbe21aa21b
Otavio Henrique <otavio.henrique@hotmail.com>
2017-08-10 21:40:49 -0300

    Function with string as predicate

--- baby.hs
+++ baby.hs
@@ -5,4 +5,5 @@ doubleSmallNumber x = (if x > 100
                         else doubleMe x) + 1 
 
 conanO'Brien = "It's a-me, Conan O'Brien!" 
-boomBangs xs = [ if x < 10 then "BOOM!" else "BANG!" | x <- xs, odd x]   
+boomBangs xs = [ if x < 10 then "BOOM!" else "BANG!" | x <- xs, odd x] 
+removeNonUppercase st = [ c | c <- st, c `elem` ['A'..'Z']]   
